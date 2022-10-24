a4ba234c7b36f9f560e444d7f8a22c35a12198fc
root <myungyuncho82@gmail.com>
2022-10-24 13:20:33 -0500

    create terraform directory

---
+++ terraform/0.12_earlier/aws/sample.tf
@@ -0,0 +1,42 @@
+resource "aws_key_pair" "web_admin" {
+  key_name = "web_admin"
+  public_key = file("~/.ssh/web_admin.pub")
+
+#  overwrite = true
+}
+
+resource "aws_security_group" "ssh" {
+  name = "allow_ssh_from_all"
+  description = "Allow SSH port from all"
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+data "aws_security_group" "default" {
+  name = "default"
+  id = "sg-07a4a2f8a94005c26"
+}
+
+resource "aws_instance" "web" {
+  ami = "ami-0d593311db5abb72b" # amzn2-ami-hvm-2.0.20200207.1-x86_64-gp2
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.web_admin.key_name
+  vpc_security_group_ids = [
+    aws_security_group.ssh.id,
+    data.aws_security_group.default.id
+  ]
+}
+
+resource "aws_db_instance" "web_db" {
+  allocated_storage    = 8
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  username             = "admin"
+  password = "<AWS_DB_PASSWORD>"
+  skip_final_snapshot = true
+}
